efbeff010fa14e97eff3157780f26afc70614b7c
alice <castaldialicee@gmail.com>
2024-01-19 15:16:46 +0100

    Aggiungere i file di progetto.

---
+++ es compito/Cane.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_compito
+{
+    internal class Cane
+    {
+        public string Razza { get; set; }
+        public string Nome { get; set;}
+        public Persona Proprietario { get; set; }   
+    }
+}
---
+++ es compito/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_compito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cane cane = new Cane();
+            cane.Nome = "Ringhio";
+            cane.Razza = "labrador";
+            cane.Proprietario.Nome = "Simone";
+            
+            //Persona p= new Persona();
+            //p.Nome = "Simone";
+        }
+    }
+}
